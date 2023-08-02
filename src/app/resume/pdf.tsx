e008af031e6969e5e2ab14729c6ded5e396b28c1
unknown <chhabrahardik345@gmail.com>
2023-08-02 18:18:52 +0530

    resume

--- src/app/resume/pdf.tsx
+++ src/app/resume/pdf.tsx
@@ -1,28 +1,35 @@
 "use client";
-import React, { useState } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
-// import "./pdf.css";
+import React from "react";
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import packageJson from "../../../package.json";
+import { useRouter } from "next/navigation";
 
-//PDFjs worker from an external cdn
 const url =
   "https://te-bucket-test.s3.ap-south-1.amazonaws.com/Hardik+Chhabra-Resume+(New).pdf";
 
 export default function Resume() {
-  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }: any) {
-    setNumPages(numPages);
-    setPageNumber(1);
-  }
-
+  const navigate = useRouter();
+  const pdfjsVersion = packageJson.dependencies["pdfjs-dist"];
+  const newplugins = defaultLayoutPlugin();
   return (
     <>
       <div className="main">
-        <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
-          <Page pageNumber={pageNumber} />
-        </Document>
+        <button
+          className="my-5 py-1 px-3 hover:text-white hover:bg-black border border-black text-black"
+          onClick={() => navigate.push(url)}
+        >
+          Download Resume
+        </button>
+        <div className="max-[550px]:hidden w-full lg:w-[70%]">
+          <Worker
+            workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.js`}
+          >
+            <Viewer fileUrl={url} plugins={[newplugins]} />
+          </Worker>
+        </div>
       </div>
     </>
   );
