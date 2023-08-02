a377d08d22f08a06ac33911e0b9be9c36c0f7997
unknown <chhabrahardik345@gmail.com>
2023-08-03 02:07:48 +0530

    changes

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,16 +1,53 @@
+"use client";
 import { NextPage } from "next";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { FaOrcid, FaLinkedin, FaGithub } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Page: NextPage = () => {
+  const navigate = useRouter();
   return (
     <>
       <div className="flex flex-col md:flex-row p-5">
-        <div className="flex w-full md:w-[30%] md:flex-col md:flex justify-center md:justify-start items-center gap-x-10">
-          <img src="" className="rounded-full" alt="..." />
-          <div className="flex justify-start">
-            <div>Email</div>
-            <div>LinkedIn</div>
-            <div>GitHub</div>
-            <div>ORCiD</div>
+        <div className="flex w-full md:w-[30%] gap-5 md:flex-col md:flex justify-center md:justify-start items-center">
+          <img
+            src="https://te-bucket-test.s3.ap-south-1.amazonaws.com/Hardik-Pic.png"
+            className="rounded-full w-36 h-36 hidden md:block"
+            alt="..."
+          />
+          <div className="flex md:flex-col md:flex justify-center md:justify-start gap-x-5">
+            <Link
+              href="mailto:chhabrahardik345@gmail.com"
+              className="flex gap-1 items-center"
+            >
+              <MdEmail /> Email
+            </Link>
+            <div
+              onClick={() =>
+                navigate.push("https://www.linkedin.com/in/hardik-chhabra/")
+              }
+              className="flex gap-1 items-center"
+            >
+              <FaLinkedin />
+              LinkedIn
+            </div>
+            <div
+              onClick={() => navigate.push("https://github.com/Hardchik")}
+              className="flex gap-1 items-center"
+            >
+              <FaGithub />
+              GitHub
+            </div>
+            <div
+              onClick={() =>
+                navigate.push("https://orcid.org/0000-0003-4943-833X")
+              }
+              className="flex gap-1 items-center"
+            >
+              <FaOrcid />
+              ORCiD
+            </div>
           </div>
         </div>
         <div className="w-full"></div>
